02fe80872c8fa6a4c504cce8735327eecc2bb5ed
n2ai <longhai2511@gmail.com>
2024-01-22 20:40:26 -0600

    add loginPage

---
+++ warehouse-FE/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/loginPage.css'
+
+const LoginPage:React.FC = () => {
+    return (
+        <>
+            <h1 className=''>Login Page</h1>
+            <input>
+            </input>
+        </>
+    )
+}
+
+export default LoginPage
