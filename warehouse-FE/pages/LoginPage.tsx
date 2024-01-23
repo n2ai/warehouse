0c1069355ebf94246f59ff8705cbf765e5b3757f
n2ai <longhai2511@gmail.com>
2024-01-22 21:49:08 -0600

    Update Backend

--- warehouse-FE/pages/LoginPage.tsx
+++ warehouse-FE/pages/LoginPage.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../styles/loginPage.css'
+import { useState } from 'react'
+
+interface ILoginForm{
+    username:string,
+    password:string
+}
 
 const LoginPage:React.FC = () => {
-    return (
-        <>
-            <h1 className=''>Login Page</h1>
-            <input>
-            </input>
-        </>
-    )
+
+    <div></div>
 }
 
 export default LoginPage
